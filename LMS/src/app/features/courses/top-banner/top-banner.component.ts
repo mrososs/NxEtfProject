6a1c0ef5294d3735019707a3eec1ea8cb0858b5a
Mohamed Osama <mohamed.designer@gmail.com>
2025-06-21 02:33:41 +0300

    add routing in courses

--- LMS/src/app/features/courses/top-banner/top-banner.component.ts
+++ LMS/src/app/features/courses/top-banner/top-banner.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-top-banner',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './top-banner.component.html',
 })
-export class TopBannerComponent {}
+export class TopBannerComponent {
+  scrollToDreams() {
+    const element = document.getElementById('dreamsSection');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
